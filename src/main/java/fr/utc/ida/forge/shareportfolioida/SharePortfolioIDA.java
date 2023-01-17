6934bae53d887a90ab882bd3954888c742f4474a
Th-Denis <106232981+Th-Denis@users.noreply.github.com>
2023-01-17 10:58:33 +0100

    Add files via upload

---
+++ src/main/java/fr/utc/ida/forge/shareportfolioida/SharePortfolioIDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.utc.ida.forge.shareportfolioida;
+
+/**
+ *
+ * @author T.D
+ */
+public class SharePortfolioIDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
